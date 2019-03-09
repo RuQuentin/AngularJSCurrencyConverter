eebdbdc1df1ae198d90cbda7fc7e86cefaf4edda
unknown <pan_rostislav@ukr.net>
2019-03-09 22:20:57 +0200

    finish markup, add routing

---
+++ src/app/pages/sign-in/sign-in.controller.js
@@ -0,0 +1,19 @@
+function SignInController($scope) {
+  'ngInject';
+
+  $scope.signInUser = () => {
+        // const username = 'dasf';
+        // const password = '123456';
+        // const auth = $firebaseAuth();
+        // auth.$signInWithEmailAndPassword(username, password)
+        //   .then(function(user) {
+        //       // eslint-disable-next-line no-console
+        //       console.log(user.uid);
+        //   }).catch(function(err){
+        //       // eslint-disable-next-line no-console
+        //       console.log(err);
+        //   }); 
+      };
+}
+
+export default SignInController;
