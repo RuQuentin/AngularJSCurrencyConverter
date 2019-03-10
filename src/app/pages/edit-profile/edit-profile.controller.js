0adf4ddf4683225e09e950131ab92ed006027732
Vladislav <vladislav.shvorobuk@gmail.com>
2019-03-10 12:29:25 +0200

    feature/edit-profile 1) add select for ava and styles for it 2) add function createFormInfo to user-profile.service.js 3) start to work on function onFileChanged (edit-profile.controller)

--- src/app/pages/edit-profile/edit-profile.controller.js
+++ src/app/pages/edit-profile/edit-profile.controller.js
@@ -1,28 +1,36 @@
 'use strict';
 
-function EditProfileController($log, $scope, $rootScope, $state) {
+function EditProfileController($log, $scope, $rootScope, userProfileService, $state) {
   'ngInject';
   $scope.success = false;
-
-  $scope.formInfo = {
-    firstName: $rootScope.currentUserInfo.firstName,
-    lastName: $rootScope.currentUserInfo.lastName,
-    role: $rootScope.currentUserInfo.role,
-    phone: $rootScope.currentUserInfo.phone,
-    email: $rootScope.currentUserInfo.email
-  };
+  $scope.formInfo = userProfileService.createFormInfo($rootScope);
   
-  $scope.submitForm = function(data) {
+  $scope.submitForm =  function(data){
     if ($scope.profile.$valid) {
       $rootScope.currentUserInfo = data;
       $scope.success = true;
       setTimeout(function(){
         $state.go('profile');
-      },1700);
+      }, 1500);
     }
   }
 
-  $log.debug('Hello from EDIT-PROFILE controller!' + $scope.first);
+  $scope.onFileChanged = function(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+    // eslint-disable-next-line no-console
+    console.log(file);
+        // eslint-disable-next-line no-console
+    console.log( $scope.formInfo.ava );
+    // в процессе реализации
+    // this.http.post('my-backend.com/file-upload', file);
+
+  }
+
+  $log.debug('Hello from EDIT-PROFILE controller!');
 }
 
 export default EditProfileController;
