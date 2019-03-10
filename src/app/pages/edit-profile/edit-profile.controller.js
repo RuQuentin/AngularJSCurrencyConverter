d17f6f0a3e70649cf63ca81a7a0a43beab786627
Vladislav <vladislav.shvorobuk@gmail.com>
2019-03-10 13:08:56 +0200

    feature/edit-profile add ability to save currentUserInfo to firebase

--- src/app/pages/edit-profile/edit-profile.controller.js
+++ src/app/pages/edit-profile/edit-profile.controller.js
@@ -1,16 +1,20 @@
 'use strict';
 
-function EditProfileController($log, $scope, $rootScope, userProfileService, $state) {
+function EditProfileController($log, $scope, $rootScope, userProfileService, usersMocksService, syncDataService, $state) {
   'ngInject';
   $scope.success = false;
   $scope.formInfo = userProfileService.createFormInfo($rootScope);
+  // syncDataService.getUserInfoFromFirebase(usersMocksService.currentUserId);
   
   $scope.submitForm =  function(data){
     if ($scope.profile.$valid) {
       $rootScope.currentUserInfo = data;
+      usersMocksService.currentUserInfo = data;
+      syncDataService.saveUserInfoToFirebase(usersMocksService.currentUserId);
       $scope.success = true;
       setTimeout(function(){
         $state.go('profile');
+        // syncDataService.getUserInfoFromFirebase();
       }, 1500);
     }
   }
@@ -23,11 +27,11 @@ function EditProfileController($log, $scope, $rootScope, userProfileService, $st
     }
     // eslint-disable-next-line no-console
     console.log(file);
-        // eslint-disable-next-line no-console
-    console.log( $scope.formInfo.ava );
-    // в процессе реализации
-    // this.http.post('my-backend.com/file-upload', file);
 
+    // в процессе реализации
+    // 1) загрузить файл в файрбейс, типа так $http('my-backend.com/file-upload', file);
+    // 2) получить его src
+    // 3) присвоить src в formInfo.ava
   }
 
   $log.debug('Hello from EDIT-PROFILE controller!');
