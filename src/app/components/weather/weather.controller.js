35187c50e50bdbba6ba76e24a79f098107d93bf0
Denis Derkach <podstavka128@gmail.com>
2019-03-13 19:14:09 +0200

    feature/weather, use destructing in setWeatherData

--- src/app/components/weather/weather.controller.js
+++ src/app/components/weather/weather.controller.js
@@ -50,13 +50,16 @@ export default class WeatherController {
     }
 
     setWeatherData(data) {
-      this.weatherData.temp = data.temp;
-      this.weatherData.feelLike = data.app_temp;
-      this.weatherData.wind = data.wind_spd;
-      this.weatherData.dir = data.wind_cdir_full;
-      this.weatherData.city = data.city_name;
-      this.weatherData.country = data.country_code;
+      const {
+        temp: temp, 
+        app_temp: feelLike, 
+        wind_spd: wind, 
+        wind_cdir_full: dir, 
+        city_name: city, 
+        country_code: country
+      } = data;
 
+      this.weatherData = {temp, feelLike, wind, dir, city, country};
       this.tableShow = true;
     }
 
