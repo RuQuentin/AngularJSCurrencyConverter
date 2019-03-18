adcbbf04c826fa100b279867ef18f5c35e03319e
Denis Derkach <podstavka128@gmail.com>
2019-03-18 16:30:29 +0200

    fix/admin-page, improve modal with user history: - improve controller - use custom-table directive

--- src/app/components/admin-modal/admin-modal.controller.js
+++ src/app/components/admin-modal/admin-modal.controller.js
@@ -4,10 +4,21 @@ function AdminModalController(sharedAdminFactory, $scope, syncDataService) {
     'ngInject';
 
     const self = this;
+    this.headers = ['Amount From', 'Amount To', 'Commission', 'Currency From', 'Currency To', 'Date', 'Rate'];
+
+    this.sort = {
+        sortingOrder: 'date',
+        reverse: false
+    }
     
     $scope.$watch(() => sharedAdminFactory.userData, () => {
-        // const userID = sharedAdminFactory.getUserData();
-        const userID = '3vesXZTo6tYLgevTBY3aLdZ26TB2';
+        const userID = sharedAdminFactory.getUserData();
+
+        if (!userID) {
+            self.data = null;
+            return;
+        }
+
         self.data = syncDataService.getCheckedUserDealsFromFirebase(userID);
     });
 }
